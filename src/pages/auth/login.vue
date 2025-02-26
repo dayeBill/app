e0a4041215f759f163320599ad3280bbc2f6c55e
liushoukun <liushoukun66@gmail.com>
2025-02-26 23:24:52 +0800

    feat(auth): 添加登录页面
    - 新增 login.vue 文件
    - 设置页面布局为 base
    - 配置导航栏标题为 "登录"- 禁用下拉刷新功能

---
+++ src/pages/auth/login.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    login
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<route  lang="json">
+{
+"layout":"base",
+"style": {
+"navigationStyle": "default",
+"navigationBarTitleText": "登录",
+"enablePullDownRefresh":false
+}
+}
+</route>
